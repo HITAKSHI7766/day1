c85276cb5547e99070b038b8b7b6c942c1f53b56
HITAKSHI7766 <hitakshigoswami7766@gmail.com>
2025-11-18 16:18:43 +0530

    Add files via upload

---
+++ exa.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() 
+{
+    printf("=======================================================\n");
+    printf("| \t Simple Blood Bank Management System \t | \n");
+    printf("=======================================================\n");
+    printf(" \t     :: Enter Your Choice ::      \n\n");
+  
+    printf(" \t    To Add New Data        \t(1)\n");
+    printf(" \t    To View List Of Donar   \t(2)\n");
+    printf(" \t    To Search Donar          \t(3)\n");
+    printf(" \t    To Exit                   \t(4)\n");
+    printf(" \t    : \n");
+  
+
+
+    return 0;
+}
