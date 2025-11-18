f15e81701780af4aebe114d7d8fb12a6d73b68ca
HITAKSHI7766 <hitakshigoswami7766@gmail.com>
2025-11-18 16:14:52 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("********************** YOUR RESULT ********************** \n");
+    printf("COLLEGE: Banasthali University, jaipur\n");
+    printf("NAME: Komal Agarwal\n");
+    printf("ROLL NUMBER: JHDU1234\n");
+    printf("SUBJECTS \t Marks1 \t Marks2 \t Total \n");
+    printf("CH-102   \t  15     \t  40     \t 55    \n");
+    printf("CH-102   \t  16     \t  36     \t 48    \n");
+    printf("CH-111   \t  20     \t  48     \t 64    \n");
+    printf("CH-107   \t  12     \t  49     \t 49    \n");
+    printf("CH-101   \t  19     \t  39     \t 61    \n");
+    printf("CH-101   \t  40     \t  50     \t 60    \n");
+    printf("TOTAL MARKS:356   \t   RESULT: PASS");
+ 
+
+    return 0;
+}
